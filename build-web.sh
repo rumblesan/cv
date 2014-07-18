b97cdb6f2e50ec5ef7a6119324b694b5604ff0d9
Guy John <guy@rumblesan.com>
2014-07-18 13:44:51 +0100

    Escaping properlly

--- build-web.sh
+++ build-web.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-latex "\def\iswebpage{1} \input{cv.tex}"
-latex "\def\iswebpage{1} \input{cv.tex}"
-latex "\def\iswebpage{1} \input{cv.tex}"
+latex '\def\iswebpage{1} \input{cv.tex}'
+latex '\def\iswebpage{1} \input{cv.tex}'
+latex '\def\iswebpage{1} \input{cv.tex}'
 tex4ht cv.tex
 
